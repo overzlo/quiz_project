78e4f30f78cd2ca46781734c3d15fb89540e3348
overzlo <flashzed_969@mail.ru>
2023-06-20 11:49:29 +0600

    first steps

---
+++ quiz_questions.js
@@ -0,0 +1,23 @@
+const quiz_questions = [
+    {
+        question: "Who is Aurora Aknes?",
+        answer : "singer",
+        options: [
+            "singer",
+            "writer",
+            "sportsman",
+            "actor"
+        ]
+    },
+    {
+        question: "The name of the first album Aurora",
+        answer : "All My Demons Greeting Me as a Friend",
+        options: [
+            "The Gods We Can Touch",
+            "Infections of a Different Kind (Step 1)",
+            "A Different Kind of Human (Step 2)",
+            "All My Demons Greeting Me as a Friend"
+        ]
+    },
+
+]
